3ef2089c5e73425c2468254f390a88b6be53db14
ryutaroM <arcticmonkey326@gmail.com>
2026-04-20 22:02:25 +0900

    feat: - ch1/ping_with_args - ch1/show_args

---
+++ ch1/ping_with_args.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+SCRIPT_NAME="${0}"
+TARGET="${1}"
+
+echo "Running the script ${SCRIPT_NAME}..."
+echo "Pinging the target ${TARGET}..."
+ping "${TARGET}"
---
+++ ch1/show_args.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "The arguments are: $@"
+echo "The total number of arguments is: $#"
